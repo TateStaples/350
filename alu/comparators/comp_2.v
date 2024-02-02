e918daff3151bd1dc01e244442056b840d4d519b
tatestaples <tatestaples@icloud.com>
2024-02-02 12:02:46 -0500

    updates

--- alu/comparators/comp_2.v
+++ alu/comparators/comp_2.v
@@ -8,15 +8,16 @@ module comp_2(EQ1, GT1, A, B, EQ0, GT0);
     wire[1:0] notA, notB;
 
     not n1(notA[1], A[1]);
-    not n1(notA[0], A[0]);
-    not n1(notB[1], B[1]);
-    not n1(notB[0], B[0]);
+    not n2(notA[0], A[0]);
+    not n3(notB[1], B[1]);
+    not n4(notB[0], B[0]);
 
-    wire selectBits[2:1];
+    wire [2:0] selectBits;
 
-    assign selectBits[2] = A[1];
-    assign selectBits[1] = A[0];
-    assign selectBits[0] = B[1];
+    assign selectBits = {A, B[1]};
+    // assign selectBits[2] = A[1];
+    // assign selectBits[1] = A[0];
+    // assign selectBits[0] = B[1];
 
 
     wire eqmux, gtmux;
